1e496b897a47d0eaf71fe6f47529784274e9ef7f
NathanYYDS <2200394635@qq.com>
2025-05-31 22:47:47 +0800

    enrich readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DataScraper
 各大平台数据抓取
+
+## 使用方法
+```bash
+# 安装依赖
+pip install requirements
+cd DataScraper
+pip install -e .
+
+# 初始化平台cookie文件
+datascraper platform --init
+```
+
+手动在platform_config.json中或者使用类的`set_cookies`方法配置平台cookie信息，然后运行test.py进行测试。
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from datascraper.platform import Platform
 zhihu = Platform("zhihu")
 
 #print(zhihu.cookies)  # 输出当前平台的 cookies
-
+#zhihu.set_cookies("your_new_cookies_here")  # 动态设置 cookies
 
 response = zhihu.get("https://zhuanlan.zhihu.com/p/52090691")
 with open("zhihu.html", "w", encoding="utf-8") as f:
